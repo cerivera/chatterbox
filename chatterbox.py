95ed0902f711db851285b3e1bdd12991d86f600d
Bzern <benzern@gmail.com>
2014-10-20 12:35:56 -0500

    Deleted \clear route
    so data can't be cleared

--- chatterbox.py
+++ chatterbox.py
@@ -83,15 +83,7 @@ class Messages(webapp2.RequestHandler):
             self.redirect('/')
 
 
-class Clear(webapp2.RequestHandler):
-    def get(self):
-        ndb.delete_multi(
-            Message.query().fetch(keys_only=True)
-        )
-        self.redirect('/')
-
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/chat', Messages),
-    ('/clear', Clear)
 ], debug=True)
